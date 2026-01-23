2c87f688139ae09e629e886b3727b9280d17bbd6
suren <surenpal666@gmail.com>
2026-01-23 22:40:29 +0900

    correcting the css

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,25 @@ function Calculater() {
   return <div className="bg-pink-100 p-5 rounded-2xl"> 
 
   <div className="grid grid-cols-4 gap-5">
-    <CalculaterBtn className="bg-yellow-200 text-black-300">C</CalculaterBtn>
+    <CalculaterBtn className="bg-yellow-200 text-black">C</CalculaterBtn>
     <CalculaterBtn >%</CalculaterBtn>
     <CalculaterBtn >√</CalculaterBtn>
-    <CalculaterBtn className="bg-orange-400 text-black-300">÷</CalculaterBtn>
+    <CalculaterBtn className="bg-orange-400 text-black">÷</CalculaterBtn>
     <CalculaterBtn>7</CalculaterBtn>
     <CalculaterBtn>8</CalculaterBtn>
     <CalculaterBtn>9</CalculaterBtn>
-    <CalculaterBtn className="bg-orange-400 text-black-300">*</CalculaterBtn>
+    <CalculaterBtn className="bg-orange-400 text-black">*</CalculaterBtn>
     <CalculaterBtn>4</CalculaterBtn>
     <CalculaterBtn>5</CalculaterBtn>
     <CalculaterBtn>6</CalculaterBtn>
-    <CalculaterBtn className="bg-orange-400 text-black-300">-</CalculaterBtn>
+    <CalculaterBtn className="bg-orange-400 text-black">-</CalculaterBtn>
     <CalculaterBtn>1</CalculaterBtn>
     <CalculaterBtn>2</CalculaterBtn>
     <CalculaterBtn>3</CalculaterBtn>
-    <CalculaterBtn className="bg-orange-400 text-black-300">+</CalculaterBtn>
+    <CalculaterBtn className="bg-orange-400 text-black">+</CalculaterBtn>
     <CalculaterBtn className="col-span-2">0</CalculaterBtn>
     <CalculaterBtn>.</CalculaterBtn>
-    <CalculaterBtn className="bg-slate-600 text-white-300">=</CalculaterBtn>
+    <CalculaterBtn className="bg-slate-600 text-white">=</CalculaterBtn>
   </div>
 
   
