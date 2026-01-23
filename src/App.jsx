9091d9bda69f94c9d5e1330f4b89faaa634fb637
suren <surenpal666@gmail.com>
2026-01-23 21:39:05 +0900

    css for the btn is created using the function calculaterbtn at the begning of the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
+function CalculaterBtn({btn}) {
+  return <button className="bg-gray-300 py-3 aspect-square rounded-2xl shadow-sm text-black-100
+    hover:brightness-95 hover:cursor-pointer active:brightness-75 transition-all duration-100">C</button>
+}
+
 function Calculater() { 
   return <div className="bg-pink-100 p-5 rounded-2xl"> 
-  
+
   <div className="grid grid-cols-4 gap-5">
-    <button className="bg-gray-300 py-3 aspect-square rounded-2xl shadow-sm text-black-100
-    hover:brightness-95 hover:cursor-pointer active:brightness-75 transition-all duration-100">C</button>
-    <button>%</button>
-    <button>√</button>
-    <button>÷</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button>*</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button>-</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>+</button>
-    <button>0</button>
-    <button>.</button>
-    <button>=</button>
+    <CalculaterBtn btn={"btn"}>C</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>%</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>√</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>÷</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>7</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>8</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>9</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>*</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>4</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>5</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>6</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>-</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>1</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>2</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>3</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>+</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>0</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>.</CalculaterBtn>
+    <CalculaterBtn btn={"btn"}>=</CalculaterBtn>
   </div>
 
   
