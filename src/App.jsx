bd1b1c4310c1d9dbdd51862f6ecc064ec76e570a
suren <surenpal666@gmail.com>
2026-01-23 22:07:55 +0900

    className is added here by ${className} and backtic is used here for calculaterBtn and class for the specfir css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-function CalculaterBtn({children}) {
-  return <button className="bg-gray-300 py-5 aspect-square rounded-2xl shadow-sm text-black-100
-    hover:brightness-95 hover:cursor-pointer active:brightness-75 transition-all duration-100 text-3xl">{children}</button>
+function CalculaterBtn({children, className}) {
+  <button
+      className={`bg-gray-300 py-5 aspect-square rounded-2xl shadow-sm text-black-100
+      hover:brightness-95 hover:cursor-pointer active:brightness-75 transition-all duration-100 text-3xl ${className}`}
+    >
+      {children}
+    </button>
+
 }
 
 function Calculater() { 
   return <div className="bg-pink-100 p-5 rounded-2xl"> 
 
   <div className="grid grid-cols-4 gap-5">
-    <CalculaterBtn>C</CalculaterBtn>
+    <CalculaterBtn className="bg-gray-400 text-yellow-500">C</CalculaterBtn>
     <CalculaterBtn>%</CalculaterBtn>
     <CalculaterBtn>√</CalculaterBtn>
     <CalculaterBtn>÷</CalculaterBtn>
