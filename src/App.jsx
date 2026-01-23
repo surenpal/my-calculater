fc5e6ed24c0925ce324306a4e7222d18e9aae966
suren <surenpal666@gmail.com>
2026-01-23 22:12:04 +0900

    seperated css for the C btn is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-function CalculaterBtn({children, className}) {
-  <button
+function CalculaterBtn({ children, className }) {
+  return (
+    <button
       className={`bg-gray-300 py-5 aspect-square rounded-2xl shadow-sm text-black-100
       hover:brightness-95 hover:cursor-pointer active:brightness-75 transition-all duration-100 text-3xl ${className}`}
     >
       {children}
     </button>
-
+  );
 }
 
 function Calculater() { 
   return <div className="bg-pink-100 p-5 rounded-2xl"> 
 
   <div className="grid grid-cols-4 gap-5">
-    <CalculaterBtn className="bg-gray-400 text-yellow-500">C</CalculaterBtn>
+    <CalculaterBtn className="bg-pink-200 text-black-300">C</CalculaterBtn>
     <CalculaterBtn>%</CalculaterBtn>
     <CalculaterBtn>√</CalculaterBtn>
     <CalculaterBtn>÷</CalculaterBtn>
